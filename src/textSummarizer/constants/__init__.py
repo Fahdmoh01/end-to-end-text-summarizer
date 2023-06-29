45dc6950046a48010c1fb6d5f89ed65d8b495f89
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-06-29 18:08:27 +0100

    completed data ingestion

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
+
+# config
+# /Users/fahdmohammed/Desktop/end-to-end-text-summarizer/config
